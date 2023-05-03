064856a6e3cfa498338514d3052e06ce2e13d4f9
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-05-03 01:27:53 -0500

    added images and selected projects for portfolio

---
+++ src/components/Portfolio/Project.js
@@ -0,0 +1,6 @@
+// Bored of my Job Board App :https://github.com/dannikaml/interactive-2
+// Ride or Die App: https://github.com/chwd31/Ride-or-Die-Cycling-App
+// Women in Aviation Blog: https://github.com/skyeflier/women-in-aviation-blog
+// Community Network: https://github.com/skyeflier/18-community-network
+// In And Out Inventory: https://github.com/skyeflier/13-in-and-out-inventory
+// Not Your Note Taker: https://github.com/skyeflier/11-not-your-note-taker
